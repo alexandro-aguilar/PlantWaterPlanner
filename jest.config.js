a8994e5353a032002d8af2293b614bc4334bbac4
Alexandro Aguilar <alex_aguilar92@icloud.com>
2026-02-28 19:08:56 -0600

    test: add coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,12 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
 };
